0d5dfd99b249b1f8403973539b86496670f4c394
ZlatanZip <45578501+ZlatanZip@users.noreply.github.com>
2020-06-22 15:33:22 +0200

    Login and Register screen initial setup (#4)
    * Login screen initial setup
    
    * Register screen initial setup

--- src/screens/Login/components/Login.js
+++ src/screens/Login/components/Login.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Login = () => {
-  return <div></div>;
-};
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      loginErrors: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = () => {
+    const { email, password } = this.state;
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Type in your email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Type in your password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
--- src/screens/Register/components/Register.js
+++ src/screens/Register/components/Register.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Register = () => {
-  return (
-    <div>
-      <h1>Register me finally hello</h1>
-    </div>
-  );
-};
+class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      password_confirmation: "",
+      registration_errors: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    const { email, password, password_confirmation } = this.state;
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password_confirmation"
+            placeholder="Enter your password"
+            value={this.state.password_confirmation}
+            onChange={this.handleChange}
+            required
+          />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Register;
