0bfbf5cc40a65e15ad65abf31e5587b14fea983a
ZlatanZip <45578501+ZlatanZip@users.noreply.github.com>
2020-06-30 20:10:24 +0200

    test (#10)

--- src/base/rootReducer.js
+++ src/base/rootReducer.js
@@ -1,9 +1,9 @@
-import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
+import {combineReducers} from "redux";
+import {reducer as formReducer} from "redux-form";
 
 import loginReducer from "../screens/Login/reducers/loginReducer";
 import registerReducer from "../screens/Register/reducers/registerReducer";
-
+//my change and three way merge
 const rootReducer = combineReducers({
   loginReducer,
   registerReducer,
